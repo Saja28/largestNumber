fe4954f643a16ef5bfc828826445f9eb8f4d6ee4
Saja28 <116615043+Saja28@users.noreply.github.com>
2025-01-09 21:42:19 -0500

    Update largestNumber.py

--- largestNumber.py
+++ largestNumber.py
@@ -1,20 +1,17 @@
 # Write a Python program to find the largest number in a list.
 a= [66,4,34,24,1,125,65,39]
-
 if a[0] < a[1] :
-    largeNum = a[1]
+    large_Num = a[1]
 else:
-    largeNum = a[0]
-
+    large_Num = a[0]
 count= 2
 while count< len(a)-1:
     count+=1
-    if a[count] < largeNum :
-        largeNum = largeNum
+    if a[count] < large_Num :
+        largeNum = large_Num
     else:
-        largeNum = a[count]
-
-print(largeNum)
+        large_Num = a[count]
+print(large_Num)
     
     
     
